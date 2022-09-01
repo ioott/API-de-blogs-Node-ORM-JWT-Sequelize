8fefdafd83577613c3e76cef741c0518102bf0ee
ioott <vioott@gmail.com>
2022-09-01 15:25:38 -0300

    config: cria a última model, postCategory
    Requisito 11 do projeto BlogPosts da Trybe

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define("PostCategory", {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+        foreignKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      foreignKey: true,
+    },
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      through: PostCategory,
+      as: 'categories',
+      foreignKey: "postId",
+      otherKey: "categoryId",
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      through: PostCategory,
+      as: 'posts',
+      foreignKey: "categoryId",
+      otherKey: "postId",
+    });
+  };
+
+  return PostCategory;
+};
