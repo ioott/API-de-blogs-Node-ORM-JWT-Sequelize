5c23d9d408214efe8e2d693ae09f70611c9970e8
ioott <vioott@gmail.com>
2022-09-01 14:00:50 -0300

    config: cria a model User
    Requisito 2 do projeto Blogs API da Trybe

---
+++ src/database/models/user.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPosts, {
+      foreignKey: "userId",
+      as: "id",
+    });
+  };
+
+  return User;
+};
